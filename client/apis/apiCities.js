249df0380d376ba3f9965fa5d947942580ba933f
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-16 11:38:21 +1300

    add api to delete city by countryId

--- client/apis/apiCities.js
+++ client/apis/apiCities.js
@@ -1,15 +1,19 @@
 import request from 'superagent'
 
-const rootUrl = '/api/v1/cities'
-
 export function getCountries() {
-  return request.get(rootUrl).then((result) => {
+  return request.get('/api/v1/cities').then((result) => {
     return result.body
   })
 }
 
 export function getCitiesByCountryId(countryId) {
-  return request.get(rootUrl + countryId).then((res) => {
+  return request.get(`api/v1/cities/${countryId}`).then((res) => {
     res.body
   })
+}
+
+export function deleteCityCountryId(countryId, cityId) {
+  return request
+    .delete(`/api/v1/cities/${countryId}/${cityId}`)
+    .then((res) => res.body)
 }
