31c90000662e3a369d4404c5bcc5500943273e2d
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-16 11:21:40 +1300

    add action to fetch cities by countryId

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,8 @@
-import { getCountries } from '../apis/apiCities'
+import { getCitiesByCountryId, getCountries } from '../apis/apiCities'
 
 export const SHOW_COUNTRIES = 'SHOW_COUNTRIES'
 export const SHOW_ERROR = 'SHOW_ERROR'
+export const SHOW_CITIES = 'SHOW_CITIES'
 
 export function receiveCountries(countries) {
   return {
@@ -22,6 +23,25 @@ export function showCountries() {
     return getCountries()
       .then((countries) => {
         dispatch(receiveCountries(countries))
+      })
+      .catch((err) => {
+        dispatch(showError(err.message))
+      })
+  }
+}
+
+export function receiveCities(cities) {
+  return {
+    type: SHOW_CITIES,
+    payload: cities,
+  }
+}
+
+export function fetchCountryCity(countryId) {
+  return (dispatch) => {
+    getCitiesByCountryId(countryId)
+      .then((cities) => {
+        dispatch(receiveCities(cities))
       })
       .catch((err) => {
         dispatch(showError(err.message))
