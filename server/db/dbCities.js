60a005c106bada9a233d7ec1c629b4420dabb1de
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-16 11:02:27 +1300

    db function to getCitiesByCountryId

--- server/db/dbCities.js
+++ server/db/dbCities.js
@@ -4,6 +4,11 @@ function getCountries(db = connection) {
   return db('countries').select()
 }
 
+function getCitiesByCountryId(countryId, db = connection) {
+  return db('cities').where('country_id', countryId).select('id', 'city')
+}
+
 module.exports = {
   getCountries,
+  getCitiesByCountryId,
 }
