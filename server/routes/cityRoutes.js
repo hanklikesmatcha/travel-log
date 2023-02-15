4d69d2e511616c75709cafde47789715406a6a67
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-16 11:06:10 +1300

    route for cities by countryId

--- server/routes/cityRoutes.js
+++ server/routes/cityRoutes.js
@@ -2,11 +2,23 @@ const express = require('express')
 const db = require('../db/dbCities')
 const router = express.Router()
 
-//GET /api/v1/cities
+//GET /api/v1/
 router.get('/', (req, res) => {
   db.getCountries()
     .then((countries) => {
       res.json(countries)
+    })
+    .catch((e) => {
+      console.error(e)
+      res.status(500).json({ message: 'Hmm, try again' })
+    })
+})
+
+//GET /api/v1/cities/:countryId
+router.get('/:countryId', (req, res) => {
+  db.getCitiesByCountryId(req.params.countryId)
+    .then((cities) => {
+      res.json(cities)
     })
     .catch((e) => {
       console.error(e)
