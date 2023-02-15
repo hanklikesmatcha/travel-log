1de263f8f54b3b0bd97aabb158f5ec33a4aebb36
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-16 11:34:02 +1300

    add db function to delete city

--- server/db/dbCities.js
+++ server/db/dbCities.js
@@ -8,7 +8,12 @@ function getCitiesByCountryId(countryId, db = connection) {
   return db('cities').where('country_id', countryId).select('id', 'city')
 }
 
+function deleteCity(id, db = connection) {
+  return db('cities').where('id', id).del()
+}
+
 module.exports = {
   getCountries,
   getCitiesByCountryId,
+  deleteCity,
 }
