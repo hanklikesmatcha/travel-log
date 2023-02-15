0db57a6aeab736c2528b89649ff31506709a3882
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-16 11:43:04 +1300

    add reducer to delete city

--- client/reducers/cities.js
+++ client/reducers/cities.js
@@ -1,9 +1,11 @@
-import { SHOW_CITIES } from '../actions'
+import { DELETE_CITY, SHOW_CITIES } from '../actions'
 
 export default function citiesReducer(state = [], action) {
   const { type, payload } = action
   switch (type) {
     case SHOW_CITIES:
+      return payload
+    case DELETE_CITY:
       return payload
     default:
       return state
