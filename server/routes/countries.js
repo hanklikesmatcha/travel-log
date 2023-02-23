87b5b875a4704ca3f42daee59232497d9a3dae15
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-23 23:38:14 +1300

    updateCountry route function

--- server/routes/countries.js
+++ server/routes/countries.js
@@ -40,4 +40,18 @@ router.delete('/:id', (req, res) => {
     })
 })
 
+//PATCH /api/v1/countries/:countryId
+router.patch('/:id', (req, res) => {
+  const countryId = Number(req.params.id)
+  const updatedCountry = req.body
+  db.updateCountry(countryId, updatedCountry.country)
+    .then(() => {
+      res.json({ id: countryId, country: updatedCountry.country })
+    })
+    .catch((e) => {
+      console.error(e)
+      res.status(500).json({ message: 'Hmm, try again' })
+    })
+})
+
 module.exports = router
