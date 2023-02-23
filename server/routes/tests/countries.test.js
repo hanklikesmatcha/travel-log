9f76c9341a393db316423ef1850409a6a4759941
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-23 23:14:45 +1300

    createCountry route function test

--- server/routes/tests/countries.test.js
+++ server/routes/tests/countries.test.js
@@ -39,7 +39,7 @@ describe('GET /', () => {
   })
 })
 
-describe('DELETE', () => {
+describe('DELETE /:countryId', () => {
   it('deletes a country', () => {
     const fakeData = [{ id: 1 }, { id: 2 }, { id: 3 }]
 
@@ -55,6 +55,30 @@ describe('DELETE', () => {
     db.deleteCountry.mockImplementation(() => Promise.reject('error'))
     return request(server)
       .delete('/api/v1/countries/2')
+      .then((res) => {
+        expect(res.status).toBe(500)
+      })
+  })
+})
+
+describe('POST /', () => {
+  it('creates an array of all countries and a new country', () => {
+    const fakeData = { country: 'Mexico' }
+
+    db.createCountry.mockReturnValue(Promise.resolve([1]))
+    db.readCountries.mockReturnValue(Promise.resolve(fakeData))
+    return request(server)
+      .post('/api/v1/countries')
+      .send(fakeData)
+      .then((res) => {
+        expect(res.status).toBe(200)
+        expect(res.body.country).toBe('Mexico')
+      })
+  })
+  it('returns 500 and logs error message when error', () => {
+    db.readCountries.mockImplementation(() => Promise.reject('error'))
+    return request(server)
+      .get('/api/v1/countries')
       .then((res) => {
         expect(res.status).toBe(500)
       })
