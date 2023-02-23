290c187bca71f2d6fbf24cebdb46d39aee250c04
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-23 20:56:42 +1300

    fix route to delete country

--- server/routes/countries.js
+++ server/routes/countries.js
@@ -2,11 +2,11 @@ const express = require('express')
 const db = require('../db/countries')
 const router = express.Router()
 
-//POST /api/v1/cities/new
+//POST /api/v1/countries/
 router.post('/', (req, res) => {
   const countryName = req.body
 
-  db.addCountry(countryName.name)
+  db.addCountry(countryName.country)
     .then(() => db.getCountries())
     .then((countries) => res.json(countries))
     .catch((e) => {
@@ -27,21 +27,17 @@ router.get('/', (req, res) => {
     })
 })
 
-// //DELETE /api/v1/countries/:countryId
-// router.delete('/:countryId', (req, res) => {
-//   const countryId = req.params.id
-
-//   db.deleteCountry(Number(countryId))
-//     .then(() => {
-//       return db.getCountries()
-//     })
-//     .then((countries) => {
-//       res.json(countries)
-//     })
-//     .catch((e) => {
-//       console.error(e)
-//       res.status(500).json({ message: 'Hmm, try again' })
-//     })
-// })
+//DELETE /api/v1/countries/:countryId
+router.delete('/:id', (req, res) => {
+  const countryId = Number(req.params.id)
+  db.deleteCountry(countryId)
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch((e) => {
+      console.error(e)
+      res.status(500).json({ message: 'Hmm, try again' })
+    })
+})
 
 module.exports = router
