3bb6c6a84901a35c3c75fc944596f2c5f48fabc7
Helen Heaji Lee <helenheajilee@gmail.com>
2023-02-28 13:22:41 +1300

    deleting country working with refresh

--- client/components/Country.jsx
+++ client/components/Country.jsx
@@ -1,21 +1,43 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-
+import { delCountry } from '../actions/countries'
 import AddCountry from './AddCountry'
 
+import { CloseButton } from '@mantine/core'
+
 function Country() {
+  const dispatch = useDispatch()
   const countries = useSelector((state) => state.countries)
+
+  function handleDelete(id) {
+    dispatch(delCountry(id))
+  }
+
   return (
     <div>
       <h2>
-        {countries.map((country) => (
-          <Link to={`/${country.id}`} key={country.id}>
-            <button>{country.country}</button>
-          </Link>
-        ))}
+        {countries &&
+          countries.map((country) => (
+            <Link to={`/${country.id}`} key={country.id}>
+              <button>{country.country}</button>
+              <CountryInfo
+                key={country.id}
+                id={country.id}
+                handleDelete={handleDelete}
+              />
+            </Link>
+          ))}
       </h2>
       <AddCountry />
+    </div>
+  )
+}
+
+function CountryInfo({ id, handleDelete }) {
+  return (
+    <div>
+      <CloseButton size="xl" iconSize={20} onClick={() => handleDelete(id)} />
     </div>
   )
 }
