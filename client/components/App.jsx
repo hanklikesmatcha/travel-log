df3824d0848d586454129347a68c6c58bb974460
Helen Heaji Lee <helenheajilee@gmail.com>
2023-03-13 20:43:29 +1300

    get rid of centering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { Center } from '@mantine/core'
+import { createStyles } from '@mantine/core'
 
 import Country from './Country'
 import Cities from './Cities'
 import Footer from './Footer'
-
+import Home from './Home'
 import { showCountries } from '../actions/countries'
 
 function App() {
@@ -19,15 +19,12 @@ function App() {
 
   return (
     <>
-      <Center style={{ width: 1400, height: 500 }}>
-        <Routes>
-          <Route path="/" element={<Country />} />
-          <Route path="/:countryId" element={<Cities />} />
-        </Routes>
-      </Center>
-      <Center style={{ width: 1400, height: 100 }}>
-        <Footer />
-      </Center>
+      <Home />
+      <Routes>
+        <Route path="/" element={<Country />} />
+        <Route path="/:countryId" element={<Cities />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
