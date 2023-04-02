b51c9f8f7d120357e1d831870db7d8e0ed9a423a
Helen Heaji Lee <helenheajilee@gmail.com>
2023-04-02 17:07:45 +1200

    not working :(

--- client/components/Country.jsx
+++ client/components/Country.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { delCountry } from '../actions/countries'
+import { delCountry, updatedCountry } from '../actions/countries'
 import AddCountry from './AddCountry'
 
 import Home from './Home'
 import Map from './Map'
-import { Button, ActionIcon } from '@mantine/core'
-import { Trash } from 'tabler-icons-react'
+import { Button, ActionIcon, Input } from '@mantine/core'
+import { Trash, Edit } from 'tabler-icons-react'
 
-function Country() {
+function Country({ country }) {
   const dispatch = useDispatch()
   const countries = useSelector((state) => state.countries)
+
+  const [editedCountry, setEditedCountry] = useState(country.country)
+  const [isEditing, setIsEditing] = useState(false)
 
   return (
     <div>
@@ -25,6 +28,29 @@ function Country() {
             <ActionIcon>
               <Trash onClick={() => dispatch(delCountry(country.id))} />
             </ActionIcon>
+            <ActionIcon>
+              <Edit onClick={() => setIsEditing(!isEditing)} />
+            </ActionIcon>
+
+            {isEditing ? (
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault()
+                  dispatch(updatedCountry(country.id, editedCountry))
+                  setIsEditing(false)
+                }}
+              >
+                <Input
+                  placeholder={country.country}
+                  value={editedCountry}
+                  onChange={(e) => {
+                    setEditedCountry(e.target.value)
+                  }}
+                />
+              </form>
+            ) : (
+              country.country
+            )}
           </Button.Group>
         ))}
       <AddCountry />
