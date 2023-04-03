580d0b662ae2e2d30e57c59bc487f10efb3109fe
Helen Heaji Lee <helenheajilee@gmail.com>
2023-04-03 12:55:03 +1200

    styling

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -6,19 +6,37 @@ import { Button, Center } from '@mantine/core'
 function Nav() {
   return (
     <>
-      <Center>
-        <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>
+      <Center style={{ marginTop: 20 }}>
+        <Button
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'cyan' }}
+          size="xl"
+          style={{ marginRight: '10px' }}
+        >
           <Link
             to="/aboutMe"
-            style={{ textDecoration: 'none', textTransform: 'uppercase' }}
+            style={{
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+              color: 'white',
+            }}
           >
             About Me
           </Link>
         </Button>
-        <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>
+        <Button
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'cyan' }}
+          size="xl"
+          style={{ marginLeft: '10px' }}
+        >
           <Link
             to="/country"
-            style={{ textDecoration: 'none', textTransform: 'uppercase' }}
+            style={{
+              textDecoration: 'none',
+              textTransform: 'uppercase',
+              color: 'white',
+            }}
           >
             Travel Log
           </Link>
