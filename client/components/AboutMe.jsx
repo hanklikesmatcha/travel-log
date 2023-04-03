879c3e4e22634c78ea0bfe0a3af1d3169aaeca39
Helen Heaji Lee <helenheajilee@gmail.com>
2023-04-03 12:40:27 +1200

    edit about me page

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 import Nav from './Nav'
 
 import { Link } from 'react-router-dom'
-import { Image, createStyles, Center, Grid } from '@mantine/core'
+import {
+  Image,
+  createStyles,
+  Center,
+  Grid,
+  useMantineTheme,
+} from '@mantine/core'
 
-const useStyles = createStyles(() => ({
+const useStyles = createStyles((theme) => ({
   header: {
     fontFamily: 'Bayon, sans-serif',
   },
@@ -13,41 +19,52 @@ const useStyles = createStyles(() => ({
   para: {
     fontFamily: 'Monaco, Courier, monospace',
   },
+  container: {
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[7]
+        : theme.colors.blue[1],
+    minHeight: '100vh',
+    padding: theme.spacing.xl,
+  },
 }))
+const ImageWrapper = ({ src, alt, caption }) => {
+  const theme = useMantineTheme()
+  const { classes } = useStyles({ theme })
+
+  return (
+    <Grid.Col span={4}>
+      <Image
+        maw={360}
+        mx="auto"
+        radius="md"
+        src={src}
+        alt={alt}
+        caption={caption}
+        className={classes.image}
+      />
+    </Grid.Col>
+  )
+}
 
 export default function App() {
-  const { classes } = useStyles()
+  const theme = useMantineTheme()
+  const { classes } = useStyles({ theme })
+
   return (
-    <>
+    <div className={classes.container}>
       <Nav />
       <Center>
         <h1 className={classes.header}>Welcome to my portfolio!</h1>
       </Center>
       <Grid>
-        <Grid.Col span={4}>
-          <Image
-            maw={360}
-            mx="auto"
-            radius="md"
-            src="/1.jpeg"
-            alt="Helen in NYC"
-            caption="Helen in NYC"
-          />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Image
-            maw={360}
-            mx="auto"
-            radius="md"
-            src="/4.JPG"
-            alt="Helen, the Best Dancer"
-            caption="Helen, the Best Dancer"
-          />
-        </Grid.Col>
-        <Image
-          maw={360}
-          mx="auto"
-          radius="md"
+        <ImageWrapper src="/1.jpeg" alt="Helen in NYC" caption="Helen in NYC" />
+        <ImageWrapper
+          src="/4.JPG"
+          alt="Helen, the Best Dancer"
+          caption="Helen, the Best Dancer"
+        />
+        <ImageWrapper
           src="/5.JPG"
           alt="Helen Bullying the Bull"
           caption="Helen Bullying the Bull"
@@ -67,27 +84,26 @@ export default function App() {
       </Center>
 
       <Center>
-        <h3 className={classes.para}>
+        <p className={classes.para}>
           Here&apos;s everything you need to know about me:
-          <ul>
-            <li>
-              I was born in South Korea. My time at NZ began in 2017 with my
-              family.
-            </li>
-            <li>
-              I have a bachelor&apos;s degree in Science, major in Chemistry.
-            </li>
-            <li>
-              I was working as a Quality Controller before I decided to pursue
-              my dreams to become a Software Developer in late 2022.
-            </li>
-            <li>
-              I finished an intense 15-week journey becoming a software
-              developer at Dev Academy Aotearoa and that 15 weeks changed my
-              life.
-            </li>
-          </ul>
-        </h3>
+        </p>
+        <ul className={classes.para}>
+          <li>
+            I was born in South Korea. My time at NZ began in 2017 with my
+            family.
+          </li>
+          <li>
+            I have a bachelor&apos;s degree in Science, major in Chemistry.
+          </li>
+          <li>
+            I was working as a Quality Controller before I decided to pursue my
+            dreams to become a Software Developer in late 2022.
+          </li>
+          <li>
+            I finished an intense 15-week journey becoming a software developer
+            at Dev Academy Aotearoa and that 15 weeks changed my life.
+          </li>
+        </ul>
       </Center>
 
       <Center>
@@ -108,6 +124,6 @@ export default function App() {
           Click here to visit my LinkedIn profile
         </Link>
       </Center>
-    </>
+    </div>
   )
 }
