62b27bb535daae764452deaa7a42448354692d05
Helen Heaji Lee <helenheajilee@gmail.com>
2023-04-05 16:07:52 +1200

    got rid of the underline

--- client/components/GoBack.jsx
+++ client/components/GoBack.jsx
@@ -5,8 +5,11 @@ import { Button } from '@mantine/core'
 function GoBack() {
   return (
     <>
-      <Button color="green" compact uppercase>
-        <Link to={'/country'} style={{ color: 'white' }}>
+      <Button color="yellow" compact uppercase>
+        <Link
+          to={'/country'}
+          style={{ color: 'white', textDecoration: 'none' }}
+        >
           Go back to country list
         </Link>
       </Button>
