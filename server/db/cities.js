e152e96f8aba0007c54b2551ca7f5eed61cc6315
Helen Heaji Lee <helenheajilee@gmail.com>
2023-04-13 16:08:38 +1200

    refactor

--- server/db/cities.js
+++ server/db/cities.js
@@ -1,19 +1,19 @@
 const connection = require('./connection')
 
-function createCity(countryId, city, db = connection) {
-  return db('cities').where('country_id', countryId).insert(city)
+function createCity(countryId, newCity, db = connection) {
+  return db('cities').where('country_id', countryId).insert(newCity)
 }
 
 function readCitiesByCountryId(countryId, db = connection) {
   return db('cities').where('country_id', countryId).select('id', 'city')
 }
 
-function updateCity(id, city, db = connection) {
-  return db('cities').update({ city: city }).where({ id })
+function updateCity(cityId, updatedCity, db = connection) {
+  return db('cities').update({ city: updatedCity }).where({ cityId })
 }
 
-function deleteCity(id, db = connection) {
-  return db('cities').where('id', id).del()
+function deleteCity(cityId, db = connection) {
+  return db('cities').where('id', cityId).del()
 }
 
 module.exports = {
